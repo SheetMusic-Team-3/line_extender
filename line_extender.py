7c0e8d415c29a24c1c32bd4fec043c25d747e4ed
Kian Chamine <kianchamine@Kians-MacBook-Pro-2.local>
2021-04-08 10:06:48 -0700

    new changes

--- line_extender.py
+++ line_extender.py
@@ -1,11 +1,17 @@
 
 
-def line_extendor(file):
+def line_extender(file):
     opened = open(file, "r")
     data = [[float(n) for n in line.split()] for line in opened]
 
     for i in range(len(data)):
         data[i][1] = 0
         data[i][3] = 1
+    with open("output.txt", "w") as output:
+        for row in data:
+            s = " ".join(map(str, row))
+            output.write(s+'n')
+                
 
-        
+    return output
+
